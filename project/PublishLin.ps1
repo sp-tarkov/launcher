baace8899a92d28641659818546905a1aef99c86
CWX <cwxdev@outlook.com>
2025-09-18 23:10:26 +0100

    Fix build scripts

--- project/PublishLin.ps1
+++ project/PublishLin.ps1
@@ -1,4 +1,6 @@
 ﻿dotnet build "./SPTarkov.Core/SPTarkov.Core.csproj" -c release -p:OutputType=Library
 copy "./SPTarkov.Core/bin/Release/net9.0/MudBlazor.min.css" "./SPTarkov.Launcher/wwwroot/MudBlazor.min.css"
 copy "./SPTarkov.Core/bin/Release/net9.0/MudBlazor.min.js" "./SPTarkov.Launcher/wwwroot/MudBlazor.min.js"
-dotnet publish "./SPTarkov.Launcher/SPTarkov.Launcher.csproj" -c release --self-contained false -p:OutputType=Exe --framework net9.0 --runtime linux-x64
+dotnet publish "./SPTarkov.Launcher/SPTarkov.Launcher.csproj" -c release --self-contained false --framework net9.0 --runtime linux-x64 -p:PublishSingleFile=true
+mkdir "./Build" -Force
+copy "./SPTarkov.Launcher/bin/Release/net9.0/linux-x64/publish/SPTarkov.Launcher" "./Build/SPTarkov.Launcher.Linux"
--- project/PublishWin.ps1
+++ project/PublishWin.ps1
@@ -1,4 +1,6 @@
-﻿dotnet publish "./SPTarkov.Core/SPTarkov.Core.csproj" -c release -p:OutputType=Library
+﻿dotnet build "./SPTarkov.Core/SPTarkov.Core.csproj" -c release -p:OutputType=Library
 copy "./SPTarkov.Core/bin/Release/net9.0/MudBlazor.min.css" "./SPTarkov.Launcher/wwwroot/MudBlazor.min.css"
 copy "./SPTarkov.Core/bin/Release/net9.0/MudBlazor.min.js" "./SPTarkov.Launcher/wwwroot/MudBlazor.min.js"
-dotnet publish "./SPTarkov.Launcher/SPTarkov.Launcher.csproj" -c release --self-contained false -p:OutputType=WinExe --framework net9.0 --runtime win-x64
+dotnet publish "./SPTarkov.Launcher/SPTarkov.Launcher.csproj" -c release --self-contained false --framework net9.0 --runtime win-x64 -p:PublishSingleFile=true
+mkdir "./Build" -Force
+copy "./SPTarkov.Launcher/bin/Release/net9.0/win-x64/publish/SPTarkov.Launcher.exe" "./Build/SPTarkov.Launcher.exe"
