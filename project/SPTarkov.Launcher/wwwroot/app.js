10b9a5d82d28c5631722b023fc059b7daea85f6b
CWX <cwxdev@outlook.com>
2025-09-19 13:31:28 +0100

    Fix formatting and naming conventions

--- project/SPTarkov.Launcher/wwwroot/app.js
+++ project/SPTarkov.Launcher/wwwroot/app.js
@@ -23,8 +23,8 @@ window.onscroll = function () {
 
 window.toggleScrollButton();
 
-// Cuz Photino starts links to external pages inside the app
-// add this script to check a tag has a link to an external site and start it externally ourself
+// Because Photino starts links to external pages inside the app
+// Add this script to check a tag has a link to an external site and start it externally ourself.
 document.addEventListener('DOMContentLoaded', () => {
     document.body.addEventListener('click', function (e) {
         const target = e.target.closest('a');
