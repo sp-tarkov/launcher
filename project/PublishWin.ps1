04cb24f599e4970d129b95fe9d5b4b54a2aad8d2
Cj <161484149+CJ-SPT@users.noreply.github.com>
2025-09-22 09:50:16 -0400

    Clean build directory on publish

--- project/PublishWin.ps1
+++ project/PublishWin.ps1
@@ -1,7 +1,8 @@
 ﻿dotnet build "./SPTarkov.Core/SPTarkov.Core.csproj" -c release -p:OutputType=Library
-copy "./SPTarkov.Core/bin/Release/net9.0/MudBlazor.min.css" "./SPTarkov.Launcher/wwwroot/MudBlazor.min.css"
-copy "./SPTarkov.Core/bin/Release/net9.0/MudBlazor.min.js" "./SPTarkov.Launcher/wwwroot/MudBlazor.min.js"
+Copy-Item "./SPTarkov.Core/bin/Release/net9.0/MudBlazor.min.css" "./SPTarkov.Launcher/wwwroot/MudBlazor.min.css"
+Copy-Item "./SPTarkov.Core/bin/Release/net9.0/MudBlazor.min.js" "./SPTarkov.Launcher/wwwroot/MudBlazor.min.js"
 dotnet publish "./SPTarkov.Launcher/SPTarkov.Launcher.csproj" -c release --self-contained false --framework net9.0 --runtime win-x64 -p:PublishSingleFile=true
+Remove-Item "./Build" -Recurse -Force
 mkdir "./Build" -Force
-copy "./SPTarkov.Launcher/bin/Release/net9.0/win-x64/publish/SPTarkov.Launcher.exe" "./Build/SPTarkov.Launcher.exe"
-copy "./SPTarkov.Core/SPT_Data" "./Build/SPT_Data" -Recurse
+Copy-Item "./SPTarkov.Launcher/bin/Release/net9.0/win-x64/publish/SPTarkov.Launcher.exe" "./Build/SPTarkov.Launcher.exe"
+Copy-Item "./SPTarkov.Core/SPT_Data" "./Build/SPT_Data" -Recurse
