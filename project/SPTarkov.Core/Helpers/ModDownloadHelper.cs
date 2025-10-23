bebeb9a03b7045ddfc6346b714b80bcd04005f74
CWX <CWXDEV@outlook.com>
2025-10-23 22:04:54 +0100

    Change class to ModDownloadHelper

---
+++ project/SPTarkov.Core/Helpers/ModDownloadHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging;
+using SPTarkov.Core.Logging;
+
+namespace SPTarkov.Core.Helpers;
+
+public class ModDownloadHelper
+{
+    private ILogger<ModDownloadHelper> _logger;
+
+    public ModDownloadHelper
+    (
+        ILogger<ModDownloadHelper> logger
+    )
+    {
+        _logger = logger;
+    }
+}
