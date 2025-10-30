104ffdee09f5183ddda43dc3e63155ad72fab190
CWX <CWXDEV@outlook.com>
2025-10-30 20:03:01 +0000

    change to record and add defaults

--- project/SPTarkov.Core/Configuration/LinuxSettings.cs
+++ project/SPTarkov.Core/Configuration/LinuxSettings.cs
@@ -1,7 +1,10 @@
 namespace SPTarkov.Core.Configuration;
 
-public class LinuxSettings
+public record LinuxSettings
 {
-    public string PrefixPath { get; set; } = "";
-    public string UmuPath { get; set; } = "";
+    // "PrefixPath": "/home/cwx/Games/tarkov",
+    public string PrefixPath { get; set; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), @"Games/tarkov");
+
+    // "UmuPath": "/home/cwx/.local/share/spt-additions/runtime/umu-run"
+    public string UmuPath { get; set; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), @".local/share/spt-additions/runtime/umu-run");
 }
