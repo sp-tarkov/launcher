e659cdf9049a566260821e8fc8d5373eaa970c1a
CWX <CWXDEV@outlook.com>
2025-10-31 16:30:43 +0000

    Update comment and pathing

--- project/SPTarkov.Core/Configuration/LinuxSettings.cs
+++ project/SPTarkov.Core/Configuration/LinuxSettings.cs
@@ -2,9 +2,12 @@ namespace SPTarkov.Core.Configuration;
 
 public record LinuxSettings
 {
-    // "PrefixPath": "/home/cwx/Games/tarkov",
-    public string PrefixPath { get; set; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), @"Games/tarkov");
+    // /home/cwx/Games/tarkov
+    public string? PrefixPath { get; set; } = Directory.GetCurrentDirectory().Split("drive_c").FirstOrDefault();
 
-    // "UmuPath": "/home/cwx/.local/share/spt-additions/runtime/umu-run"
+    // /home/cwx/.local/share/spt-additions/runtime/umu-run
     public string UmuPath { get; set; } = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), @".local/share/spt-additions/runtime/umu-run");
+
+    // ENVVAR1=1 ENVVAR2=2 %command% -Arg1=arg1 -Arg2=arg2 - similar system to steam
+    public string LaunchSettings { get; set; } = "";
 }
