585c4446d41d260dbcf939150793d5d72a262ee7
CWX <cwxdev@outlook.com>
2025-11-11 18:28:28 +0000

    naming go brr

---
+++ project/SPTarkov.Core/Forge/ForgeListOfLinks.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SPTarkov.Core.Forge;
+
+public class ForgeListOfLinks
+{
+    [JsonPropertyName("url")]
+    public string Url { get; set; }
+
+    [JsonPropertyName("label")]
+    public string Label { get; set; }
+}
