d0074e24644e9eba1dfce34a833d1db81885f841
CWX <cwxdev@outlook.com>
2025-11-29 21:58:07 +0000

    add update task

---
+++ project/SPTarkov.Core/Mods/UpdateTask.cs
@@ -0,0 +1,14 @@
+﻿namespace SPTarkov.Core.Mods;
+
+public class UpdateTask
+{
+    public string ModName { get; set; }
+    public string Version { get; set; }
+    public string GUID { get; set; }
+    public string Link { get; set; }
+    public float Progress { get; set; }
+    public float TotalToDownload { get; set; }
+    public required CancellationTokenSource CancellationToken { get; set; }
+    public bool Complete { get; set; }
+    public Exception Error { get; set; }
+}
