a7feb6b97105079441a23b74fe08572e24875a04
CWX <cwxdev@outlook.com>
2026-01-10 15:44:27 +0000

    update proton

--- project/SPTarkov.Core/Configuration/LinuxSettings.cs
+++ project/SPTarkov.Core/Configuration/LinuxSettings.cs
@@ -6,13 +6,15 @@ public record LinuxSettings
     public string? PrefixPath { get; set; } = Directory.GetCurrentDirectory().Split("/drive_c").FirstOrDefault();
 
     // /home/cwx/.local/share/spt-additions/runtime/umu-run
+    // /usr/bin/umu-run
+    // TODO: this needs fixing, can be multiple places - Maybe Madbyte can copy umu-run to additions
     public string UmuPath { get; set; } = Path.Join(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), @".local/share/spt-additions/runtime/umu-run");
 
     // ENVVAR1=1 ENVVAR2=2,yes=1 -Arg1="arg1 space" -Arg2=arg2 - similar system to steam
     public string LaunchSettings { get; set; } = "";
 
     // "GE-Proton10-24" "GE-Proton10-20"
-    public string ProtonVersion { get; set; } = "GE-Proton10-26";
+    public string ProtonVersion { get; set; } = "GE-Proton10-28";
 
     public bool GameMode { get; set; } = false;
 }
