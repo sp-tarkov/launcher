2f2d47648ea908fcacec9e46ee1cdef40eaa3c0b
CWX <cwxdev@outlook.com>
2026-06-11 23:04:31 +0100

    Add new response model

---
+++ project/SPTarkov.Core/Forge/Responses/ForgeAddonDetailsResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SPTarkov.Core.Forge.Responses;
+
+public class ForgeAddonDetailsResponse
+{
+    [JsonPropertyName("success")]
+    public bool Success { get; init; }
+
+    [JsonPropertyName("data")]
+    public ForgeBase? Data { get; init; }
+}
