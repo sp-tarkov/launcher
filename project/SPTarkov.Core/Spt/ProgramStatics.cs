b6057239657c0c6cf365a2640e083a5b4d3b7baa
CWX <cwxdev@outlook.com>
2026-06-11 23:05:02 +0100

    Add ProgramStatics like the server to tie a version

---
+++ project/SPTarkov.Core/Spt/ProgramStatics.cs
@@ -0,0 +1,11 @@
+﻿using Version = SemanticVersioning.Version;
+
+namespace SPTarkov.Core.SPT;
+
+public static partial class ProgramStatics
+{
+    public static Version SptVersionCompiledFor
+    {
+        get { return SptVersion; }
+    }
+}
