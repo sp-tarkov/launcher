794570d2c5d25f69cec1c054d9e07b6b7bb9a465
CWX <cwxdev@outlook.com>
2026-07-01 23:25:29 +0100

    add new forge models

---
+++ project/SPTarkov.Core/Forge/ForgeCategory.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SPTarkov.Core.Forge;
+
+public class ForgeCategory
+{
+    [JsonPropertyName("id")]
+    public int? Id { get; set; }
+
+    [JsonPropertyName("hub_id")]
+    public int? HubId { get; set; }
+
+    [JsonPropertyName("title")]
+    public string? Title { get; set; }
+
+    [JsonPropertyName("slug")]
+    public string? Slug { get; set; }
+
+    [JsonPropertyName("description")]
+    public string? Description { get; set; }
+}
---
+++ project/SPTarkov.Core/Forge/Responses/ForgeCategoriesResponse.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace SPTarkov.Core.Forge.Responses;
+
+public class ForgeCategoriesResponse
+{
+    [JsonPropertyName("success")]
+    public bool Success { get; set; }
+
+    [JsonPropertyName("data")]
+    public List<ForgeCategory>? Data { get; set; }
+
+    [JsonPropertyName("links")]
+    public ForgeLinks? Links { get; init; }
+
+    [JsonPropertyName("meta")]
+    public ForgeMeta? Meta { get; init; }
+}
